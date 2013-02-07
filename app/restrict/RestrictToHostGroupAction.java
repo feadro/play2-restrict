12435363851f2a604e65e2c20f1b47ab4f0af5ac
Franz Granlund <franz.granlund@gmail.com>
2013-02-07 15:09:24 +0200

    Minor code refactor.

--- app/restrict/RestrictToHostGroupAction.java
+++ app/restrict/RestrictToHostGroupAction.java
@@ -7,17 +7,16 @@ import play.mvc.Action;
 import play.mvc.Http;
 import play.mvc.Result;
 
-import java.net.InetAddress;
 import java.util.List;
 
 import static play.Play.application;
 
 public class RestrictToHostGroupAction extends Action<RestrictToHostGroup> {
     public static final String RESTRICT_ALREADY_SET = "restricttohostgroup-set";
-    public static final String DEFAULT = "default";
+    public static final String CONFIG_KEY_DEFAULT = "default";
     public static final String CONFIG_KEY_GROUPS = "restricttohostgroup.groups.";
     public static final String CONFIG_KEY_DENIED_REDIRECT = "restricttohostgroup.redirect";
-    public static final String MSG_GROUP_NOT_FOUND = "RestrictToHostGroup - Did not find group '%s'. Falling back to 'default'";
+    public static final String MSG_GROUP_NOT_FOUND = "RestrictToHostGroup - Group '%s' not found. Using 'default'.";
     public static final String MSG_LOG_ACCESS_DENIED = "RestrictToHostGroup - Access denied for %s [%s]";
 
     @Override
@@ -26,7 +25,7 @@ public class RestrictToHostGroupAction extends Action<RestrictToHostGroup> {
             ctx.args.put(RESTRICT_ALREADY_SET, "");
 
             String value = configuration.value();
-            String group = (value == null || value.isEmpty()) ? DEFAULT : value;
+            String group = (value == null || value.isEmpty()) ? CONFIG_KEY_DEFAULT : value;
             String remoteAddress = trimRemoteAddress(ctx.request().remoteAddress());
 
             /**
@@ -35,7 +34,7 @@ public class RestrictToHostGroupAction extends Action<RestrictToHostGroup> {
              */
             if(!application().configuration().keys().contains(CONFIG_KEY_GROUPS + group)) {
                 Logger.warn(String.format(MSG_GROUP_NOT_FOUND, group));
-                group = DEFAULT;
+                group = CONFIG_KEY_DEFAULT;
             }
 
             /**
